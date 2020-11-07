7d21af479fec83893c4f834f4e3d1d3943ff2cfa
Unanyan <meruzhan.unanyan@bk.ru>
2020-11-08 03:09:29 +0400

    Added product model and CKEditor

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,5 +1,29 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=80)
+    MEN = 'M'
+    WOMEN = 'W'
+    CHILD = 'C'
+    TYPE_CHOICES = [
+        (MEN, 'Men'),
+        (WOMEN, 'Women'),
+        (CHILD, 'Child'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, default='')
+    content = RichTextField(blank=True) #null=False, default='')
+    is_slider_item = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="images/")
+    price = models.IntegerField(default=0)
+    count = models.IntegerField(default=0)
+    type = models.CharField(
+        max_length=1,
+        choices=TYPE_CHOICES,
+        default=CHILD,
+    )
+
+    def __str__(self):
+        return self.name
--- shop/product/views.py
+++ shop/product/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import get_list_or_404
+
+from product.models import Product
 
 
 def index(request):
-    return HttpResponse("Our products!!!")
+    products = get_list_or_404(Product)
+    print(products)
+    return HttpResponse(products)
